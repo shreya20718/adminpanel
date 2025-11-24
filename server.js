5916d7e8a10153e2e322804cea5c5468e55c5ce4
shreya <shreyagaikwad107@gmail.com>
2025-11-24 15:45:45 +0530

    good

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, "public")));
 
 // Fallback for SPA routing
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
